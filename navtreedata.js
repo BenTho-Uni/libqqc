5854ec1371d5c5e2221419c5222a21b8bb2adaf4
BenTho-Uni <BenTho-Uni@users.noreply.github.com>
2022-02-22 12:25:43 +0000

    deploy: e10d4abeaa5099aea024cce03968d87f4dfb3982

--- navtreedata.js
+++ navtreedata.js
@@ -38,11 +38,8 @@ var NAVTREE =
     [ "Installation", "index.html#autotoc_md9", null ],
     [ "Usage", "index.html#autotoc_md10", null ],
     [ "FAQ", "index.html#autotoc_md11", null ],
-    [ "Roadmap", "index.html#autotoc_md12", [
-      [ "done", "index.html#autotoc_md13", null ],
-      [ "ToDo", "index.html#autotoc_md14", null ]
-    ] ],
-    [ "Credits", "index.html#autotoc_md15", null ],
+    [ "ToDo", "index.html#autotoc_md12", null ],
+    [ "Credits", "index.html#autotoc_md13", null ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
