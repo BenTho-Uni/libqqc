16d2347747a8b51208b7222b35a19ca500af55fc
BenTho-Uni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2022-03-01 10:05:24 +0100

    fixed make_run script for onlympi runs

--- runs/make_run_1xX.sh
+++ runs/make_run_1xX.sh
@@ -9,16 +9,19 @@ else
 fi
 
 echo '#!/bin/bash'
-echo '#SBATCH -J jove_mpi_test_1x'$NCORES
-echo '#SBATCH -N1 --ntasks=1 --cpus-per-task='$NCORES '--mem-per-cpu=10G -t12:00:00'
-echo '#SBATCH --job-name=jove_mpi_test_1x'$NCORES
-echo '#SBATCH --output=jove_mpi_test_1x'$NCORES'_por-%j.out'
-echo '#SBATCH --error=jove_mpi_test_1x'$NCORES'_por-%j.err'
+echo '#SBATCH -J libqqc_mpionly_1x'$NCORES
+echo '#SBATCH -N1 --ntasks=1 --cpus-per-task='$NCORES '-t12:00:00'
+echo '#SBATCH --mem=100G'
+echo '#SBATCH --job-name=libqqc_mpionly_1x'$NCORES
+echo '#SBATCH --output=libqqc_mpionly_1x'$NCORES'_por-%j.out'
+echo '#SBATCH --error=libqqc_mpionly_1x'$NCORES'_por-%j.err'
+echo 'pwd; hostname; date'
 echo 'cat run_1x'$NCORES'.sh'
 echo 'module load compiler/gnu'
 echo 'module load mpi/openmpi'
-echo 'mpic++ -fopenmp -O3 -ffast-math -march=native test_mpi.C -o 1x'$NCORES'.out'
-echo 'OMP_PLACES=cores OMP_PROC_BIND=spread OMP_NUM_THREADS='$NCORES' mpirun -np 1 --bind-to core --map-by ppr:1:node:pe='$NCORES' -x OMP_NUM_THREADS -x OMP_PROC_BIND -x OMP_PLACES ./1x'$NCORES'.out'
+echo 'cd ../host_example/.'
+echo 'mpic++ -fopenmp -O3 -ffast-math -march=native main_mpi.cpp ../lib/libqqc.a -o 1x'$NCORES'.out'
+echo 'OMP_NUM_THREADS=1 srun --cpus-per-task=1 ./1x'$NCORES'.out'
 
 echo 'rm 1x'$NCORES'.out'
 
