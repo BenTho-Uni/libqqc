2cb5bc2a31d18ddef72984781f0279b1da207713
BenTho-Uni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2022-04-28 17:19:34 +0200

    added simple call to qchem_main

--- libqqc/qchem/qqc_main.cpp
+++ libqqc/qchem/qqc_main.cpp
@@ -7,6 +7,8 @@
 
 //Internal headers
 //#include <qqc.h>
+#include "../methods/do_qmp2.h"
+#include "../utils/ttimer.h"
 
 //Host headers
 #include <qchem.h>
@@ -25,7 +27,37 @@
 
 int qqc_main(){
     try {
-        std::cout << "QQC reporting in!";
+        //Set a output stream for our data
+        ostringstream out;
+        //Set up an outer timer to time the different modules
+        //loader, vault, method
+        Ttimer timings(0);
+
+        // The Loader provides method and host_program specific loading function
+        // here we load from files on disk for the Q-MP2 method
+        timings.start_new_clock("Timing Loader_qmp2_from_file:: loader :", 0, 0);
+        Loader_qmp2 loader;
+        timings.stop_clock(0);
+        cout << timings.print_clocks(0);
+
+        // The vault calls the loader functions and saves all the necessary data
+        // for this method
+        timings.start_new_clock("Timing Vault_qmp2:: vault : ", 1, 0);
+        Vault_qmp2 vault(loader);
+        timings.stop_clock(1);
+        cout << timings.print_clocks(1);
+
+        // The vault calls the loader functions and saves all the necessary data
+        // for this method
+        timings.start_new_clock("Timing Do_qmp2:: qmp2 and run : " , 2, 0);
+        Do_qmp2 qmp2(vault);
+        // And execute it
+        qmp2.run(out);
+        timings.stop_clock(2);
+        cout << timings.print_clocks(2);
+
+        // Print our output to standard out
+        cout << out.str() << endl;
     }
     catch (std::exception &e) {
         QCrashLoc(e.what(), __FILE__, __LINE__);
