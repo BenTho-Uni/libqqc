64938d78708c8264b0a19eb24f903805f999c32b
BenTho-Uni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2022-05-09 18:00:46 +0200

    started writiing loader, code not working

--- libqqc/loader/loader_qmp2_from_qchem.cpp
+++ libqqc/loader/loader_qmp2_from_qchem.cpp
@@ -1,13 +1,22 @@
 ///
 /// member functions of the loader class which loads values and returns them to
 /// a reference
-/// @TODO this is just a dummy implementation, needs to be fully implemented
 /// @file loader_qmp2_from_qchem.cpp
 /// @author Benjamin Thomitzni
 /// @version 0.1 11-01-2022
 //
 
 #include "loader_qmp2_from_qchem.h"
+
+// Q-Chem libraries
+#include <qchem.h>
+#include <libqints/basis/basis_1e1c_cgto.h>
+#include <libqints/qchem/aobasis.h>
+#include <libqints/types.h>
+#include <libqints/ftype/ftype_cgto.h>
+
+#include <libdftn/xcclass.h>
+#include <libgrid/libgrid/eval_cgto.h>
 
 #include <iostream>
 
@@ -35,26 +44,31 @@ namespace libqqc {
 
     void Loader_qmp2_from_qchem :: load_nocc (size_t &nocc){
 
-        cout << "Loader reached!\n";
-        nocc = 1; //
+        //nocc = 1; //
+        occ = rem_read(REM_NALPHA);
 
     } //Loader_qmp2_from_qchem::load_nocc
 
     void Loader_qmp2_from_qchem :: load_nvirt(size_t &nvirt) {
 
-        nvirt = 2;
+        size_t nmo = rem_read(REM_NLINOR);
+        size_t occ = 0;
+        load_nocc(occ);
+        nvirt = nmo - occ;
     
     }
 
     void Loader_qmp2_from_qchem :: load_nao(size_t &nao) {
+        libqints::basis_1e1c_cgto<double> basis = libqints::qchem::aobasis.b1;
 
-        nao = 3;
+        nao = basis.get_nbsf();
     
     }
 
     void Loader_qmp2_from_qchem :: load_prnt_lvl(int &prnt_lvl) {
 
-        prnt_lvl = 0;
+        int prnt_lvl_qchem = rem_read(REM_QQC_PRINT_LEVEL);
+        prnt_lvl = (prnt_lvl_qchem > 0) ? prnt_lvl_qchem : 0;
     
     }
 
@@ -74,23 +88,53 @@ namespace libqqc {
 
     void Loader_qmp2_from_qchem :: load_3Dgrid(Grid &grid) {
 
-        size_t npts = 2;
-        size_t ndim = 3;
-        double pts[npts * ndim];
+//        size_t npts = 2;
+//        size_t ndim = 3;
+//        double pts[npts * ndim];
+//        double wts[npts];
+
+//        fill_array (npts * ndim, pts);
+//        fill_array (npts, wts);
+
+//        grid.set_grid(npts, ndim, pts, wts);
+        
+        XCAtoms xcatom; 
+        int jobID0 = 0; 
+        XCFunctional Func(0,0); 
+        XCJobPara xcpara(Func, xcatom, jobID0);
+        double thresh = xcpara.thresh; 
+        int nAtoms = xcatom.getNAtoms();
+
+        int grdTyp = rem_read(REM_IGRDTY);
+
+        int IBCode = rem_read(REM_IBASIS);
+        SET_QC_FCOMMON(&IBCode); 
+
+        XCBasisSet basDen(IBCode, nAtoms, thresh); 
+        xcatom.setSize(basDen); 
+
+        MoleGrid mgrid(xcatom, grdTyp, xcpara.nDrvNuc, thresh);
+
+        int *iPtBat= new int[mgrid.getNBatch()+1]; 
+        int npts=mgrid.getNPts();
+        int nbatch=mgrid.getNBatch();
+
+        double pts[npts];
         double wts[npts];
 
-        fill_array (npts * ndim, pts);
-        fill_array (npts, wts);
+        mgrid.getMolGrid(pts, wts, iPtBat, npts, nbatch); 
 
-        grid.set_grid(npts, ndim, pts, wts);
+        grid.set_grid(npts, 3, pts, wts);
+
+        delete[] iPtBat; 
 
     }
 
     void Loader_qmp2_from_qchem :: load_mat_fock(double* mat_fock) {
+        size_t nsbf = 0;
+        load_nao(nbsf);
 
-        size_t nao = 3;
-
-        fill_array (nao * nao, mat_fock); 
+        FileMan(FM_READ, FILE_FOCK_MATRIX, FM_DP, nbsf * nbsf, 0, FM_BEG, mat_fock); 
 
     }
 
